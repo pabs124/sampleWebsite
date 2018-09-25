b4ce5b14f1998801f8cc45460f64c8f168f79790
Sahil Pabby <pabs124@gmail.com>
2018-09-24 23:18:56 -0400

    almost complete website

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import '../components/Header.css'
 
 const Header = ({ siteTitle }) => (
-  <div>
-    <div>
-      <h1 >
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30"/></Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">Downloads</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/buy"><button>Buy</button></Link>
     </div>
   </div>
 )
--- src/components/layout.js
+++ src/components/layout.js
@@ -28,14 +28,8 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
+        <Header />
         <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
         >
           {children}
         </div>

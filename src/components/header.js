72443272a3a18ffab4fdfd94925589d970f5172c
Sahil Pabby <pabs124@gmail.com>
2018-09-24 21:02:20 -0400

    basic css and images

--- src/components/header.js
+++ src/components/header.js
@@ -2,20 +2,9 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <div>
+    <div>
+      <h1 >
         <Link
           to="/"
           style={{
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,14 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Quick tips and tricks to master the gadgets near you</h1>
-    <p>Welcome to your Simple Tech Man Website.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Quick tips and tricks to master the gadgets near you</h1>
+        <p>Welcome to your Simple Tech Man Website.</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
   </Layout>
 )
 
